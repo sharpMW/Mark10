44f40d0ad2b5c9118859a2263a20177b13103104
sharpMW <88919653+sharpMW@users.noreply.github.com>
2021-09-14 14:59:34 +0530

    bugs fixed

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ checkButton.addEventListener("click", function validateAmount() {
 
     hideMessage();
     if (billAmount.value > 0) { 
-        if (paidAmount.value >= billAmount.value) { 
-                const returnAmount = paidAmount.value - billAmount.value;
+        if (Number(paidAmount.value) >= Number(billAmount.value)) { 
+                const returnAmount = Number(paidAmount.value) - Number(billAmount.value);
                 calculateChange(returnAmount);
         } else { 
             showMessage("Bill is not paid");
